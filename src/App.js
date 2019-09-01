eaea2247f6e4d149e4f07ecdb72254e245c29e4c
Bull-Butcher <markjamesbutcher@gmail.com>
2019-09-01 23:31:28 +1000

    search bar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component }from 'react';
+import { CardList } from './components/card-list/card-list-componet';
+import { SearchBox } from './components/search-box/search-box-component';
 import './App.css';
 
 class App extends Component {
@@ -6,10 +8,12 @@ class App extends Component {
     super();
 
     this.state = {
-      monster: []
+      monster: [],
+      searchFeild: ''
     }
   }
 
+  // API for monster profiles
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
@@ -17,15 +21,21 @@ class App extends Component {
   }
   
   render() {
+    // function to handle search bar input to be letter sensitive, to what renders in CardList
+    const { monster, searchFeild } = this.state;
+    const filteredMonsters = monster.filter(monster => 
+      monster.name.toLowerCase().includes(searchFeild.toLowerCase()))
     return (
       <div className="App">
-        {
-          this.state.monster.map( monster => <h1 key={monster.id}> { monster.name} </h1>)
-        }
+        <SearchBox 
+        type="search"
+        placeholder="search monsters"
+        handleChange={e => this.setState({ searchFeild: e.target.value})}
+        />
+        <CardList monster={filteredMonsters}/>
      </div>
     )
   }
-
 }
 
 export default App;
